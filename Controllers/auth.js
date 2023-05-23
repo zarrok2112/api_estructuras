ba5664b35450c900430bf3c88d5ae2f6eb831e9f
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-05-23 11:58:07 -0500

    Update auth.js
    update para que trabaje con /models/usuario
    
    funcionalidad de buscar usuarios existentes

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario')
 
 const crearUsuario = async (req, res = express.Request) => {
     const { name, email, password } = req.body;
-    console.log(req.body);
-
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-        return res.status(400).json(
-            {
+    try{
+        let usuario = await Usuario.findOne({email: email})
+        if(usuario){
+            return res.status(400).json({
                 ok: false,
-                errors: error.mapped()
-            }
-        )
-    }
-    try {
-        return res.status(200).json({
+                msg: 'El usuario con ese correo ya existe'
+            })
+        }
+        usuario = new Usuario(req.body);
+        await usuario.save();
+
+        res.status(200).json({
             ok: true,
-            msg: 'registro',
-            name,
-            email,
-            password
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado... revisar logs'
-        });
+            usuario
+        })
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            error
+        })
     }
-
-
-    // return res.status(200).json({
-    //     ok: true,
-    //     msg: 'registro',
-    //     name,
-    //     email,                                 
-    //     password
-    // });
 };
 
 const login = async(req, res = express.response) => {
