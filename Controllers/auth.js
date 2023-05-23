aba5ce2ab1f5064edd40386897dfb87379f370ba
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-05-23 12:09:50 -0500

    login - /controllers/auth.js
    funcionalidad de login

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -12,7 +12,7 @@ const crearUsuario = async (req, res = express.Request) => {
                 msg: 'El usuario con ese correo ya existe'
             })
         }
-        
+
         usuario = new Usuario(req.body);
         const salt = bcrypt.genSaltSync();
         usuario.password = bcrypt.hashSync(password, salt);
@@ -32,10 +32,37 @@ const crearUsuario = async (req, res = express.Request) => {
 };
 
 const login = async(req, res = express.response) => {
-    return res.json({
-        ok: true,
-        msg: 'login'
-    });
+    const{email, password} = req.body
+
+    try{
+
+        let usuario = await Usuario.findOne({email:email})
+        if(!usuario){
+            return res.status(400).json({
+                ok:false,
+                msg:'El usuario NO existe'
+            })
+        }
+
+        const passwordValid = bcrypt.compareSync(password, usuario.password);
+        if(!passwordValid){
+            return res.status(400).json({
+                ok:false,
+                msg:'La contraseña NO es valido'
+            })
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario
+        })
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            error
+        })
+    }
 }
 
 const renewToken = async(req, res = express.response) => {
