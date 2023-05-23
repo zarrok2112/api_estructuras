b0146c21756ef2329a1760a0722de5ebd69200d9
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-05-23 12:32:09 -0500

    Create validar-token.js
    creacion de /middleware/validar-token.js

---
+++ middleware/validar-token.js
@@ -0,0 +1,33 @@
+const {response} = require('express');
+const jwt = require('jsonwebtoken');
+
+const validarJWT = (req, res = response, next) => {
+
+    const token = req.header('x-token')
+    if(!token){
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token inexistente'
+        })
+    }
+
+    try{
+        const {uid, name} = jwt.verify(
+            token,
+            process.env.SECRET_JWT_SEED
+        )
+
+        req.uid = uid
+        req.name = name
+    } catch(error){
+        return res.status(401).json({
+            ok:false,
+            msg:'Token invalido'
+        })
+    }
+    next();
+}
+
+module.exports = {
+    validarJWT
+}
