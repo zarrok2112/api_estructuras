9809eb676e11b8303d2a963f4589b4d46a70acc0
zarrok <84244625+zarrok2112@users.noreply.github.com>
2023-05-23 19:11:18 -0500

    creacion por zarrok de controladores

---
+++ Controllers/social.js
@@ -0,0 +1,9 @@
+const addPublication = async (req, res) => { };
+const getPublications = async (req, res) => { };
+const deletePublication = async (req, res) => { };
+
+const addLike = async (req, res) => { };
+
+const addFriend = async (req, res) => { };
+const getFriends = async (req, res) => { };
+const deleteFriend = async (req, res) => { };
