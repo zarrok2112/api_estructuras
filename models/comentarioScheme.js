b0d28dfb718a2abf8cf5f97b9300c81544f447ae
zarrok <anakinvv@gmail.com>
2023-05-30 00:12:03 -0500

    creacion de comentarios

---
+++ models/comentarioScheme.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require('mongoose');
+
+const ComentarioScheme = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    usuario: {
+        type: String,
+        ref: 'Usuario',
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    dislikes: {
+        type: Number,
+        default: 0
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
+},{
+    toJSON:{
+        virtuals: true
+    },
+    toObject:{
+        virtuals: true
+    }
+})
+
+ComentarioScheme.method('toJSON', function(){
+    const {__v, _id, ...object} = this.toObject();
+    object.id = _id
+    return object
+})
+
+module.exports = model('Comentario', ComentarioScheme)
+;
